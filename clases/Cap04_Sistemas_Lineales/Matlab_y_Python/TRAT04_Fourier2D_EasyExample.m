baa7fb550b6d7447c73fc7d1c52117bcd5e79d8b
José Ramón Iglesias Gamarra <38440709+joseramoniglesias@users.noreply.github.com>
2022-08-08 14:31:24 -0500

    Add files via upload

---
+++ clases/Cap04_Sistemas_Lineales/Matlab_y_Python/TRAT04_Fourier2D_EasyExample.m
@@ -0,0 +1,13 @@
+% José Ramón Iglesias 2019
+I = zeros(512,512);
+I(240:269,240:269) = 1;
+figure(1)
+imshow(I,[])
+figure(2)
+mesh(I)
+F = fft2(I);
+S = fftshift(log(abs(F)+1));
+figure(3)
+imshow(S,[])
+figure(4)
+mesh(S)
---
+++ clases/Cap04_Sistemas_Lineales/Matlab_y_Python/TRAT04_FourierBasis.m
@@ -0,0 +1,35 @@
+% Funciones base de Fourier 2D
+% José Ramón Iglesias, Septiembre 2019
+% https://sites.google.com/a/unicesar.edu.co/jose-r-iglesias/
+
+N=16;
+d = 1;
+close all
+j=sqrt(-1);
+X = [];
+for m=0:d:N-1
+    Xf = [];
+    m
+    for n=0:d:N-1
+        x = zeros(N,N);
+        for i=1:N
+            for k=1:N
+                x(i,k) = exp(-j*2*pi/N*(m*i+n*k));
+            end
+        end
+        x0=(real(x)+1)*128;
+        imshow(x0,[]);pause(0)
+        drawnow
+        Xf = [Xf x0];
+    end
+    X = [X;Xf];
+end
+imshow(X,[])
+hold on
+N2 = size(X,1);
+for i=0:N2/N:N2
+        plot([i i]+0.5,[0 N2]+0.5,'g')
+        plot([0 N2]+0.5,[i i]+0.5,'g')
+end        
+
+                
