a49379d38f06493d5b7b2c015d4925836b38ef38
Jose Ramon Iglesias Gamarra <38440709+joseramoniglesias@users.noreply.github.com>
2022-08-09 13:23:40 -0500

    Update Readme.cmd

--- clases/Cap01_Introducción/Readme.cmd
+++ clases/Cap01_Introducción/Readme.cmd
@@ -1,1 +1,31 @@
-
+# Capitulo 01: Introduccion
+### 1.1 Introduccion al analisis y procesamiento digital de imagenes:
+* PPT - Introduccion al Procesamiento de Imagenes [[PPT]](https://github.com/domingomery/imagenes/blob/master/clases/Cap01_Introduccion/presentations/IMG01_Introduccion.pptx)
+* Colab - Ejemplo segmentacion de un arroz [[Colab]](https://colab.research.google.com/drive/17jwZY9fxKw3KUi_--sfYZrksfpR3_Whr?usp=sharing)
+* Video - Clase grabada (2021): Introduccion [[YouTube]](https://youtu.be/ou1R9UbwP00)
+* PPT - Adquisicion de imagenes [[PPT]](https://github.com/domingomery/imagenes/blob/master/clases/Cap01_Introduccion/presentations/IMG01_Adquisicion.pptx)
+* Socrative - Ejercicio E01 (ROOM = IMAGENES) [[Quiz]](http://www.socrative.com)
+### 1.2 Luz, espectro electromagnetico y elementos de percepcion visual:
+* Video - Clase grabada (2021): adquisicion, sampling, ejemplo [[YouTube]](https://youtu.be/v1g1cb-zYTU)
+* PPT - Luz, espectro, vision [[PPT]](https://github.com/domingomery/imagenes/blob/master/clases/Cap01_Introduccion/presentations/IMG01_Luz-Ondas-Ojo.pptx)
+* PPT - Ejercicio: Punto Ciego [[PPT]](https://github.com/domingomery/imagenes/blob/master/clases/Cap01_Introduccion/presentations/IMG01_EjercicioPuntoCiego.pptx)
+* Articulo - Lo ves a color o en blanco y negro? [[Articulo]](https://www.bbc.com/mundo/noticias-49178274)
+* Matlab - Ejemplo de Percepcion 1 [[Matlab]](https://github.com/domingomery/imagenes/blob/master/clases/Cap01_Introduccion/matlab/IMG01_Perception1.m)
+* Matlab - Ejemplo de Percepcion 2 [[Matlab]](https://github.com/domingomery/imagenes/blob/master/clases/Cap01_Introduccion/matlab/IMG01_Perception2.m)
+* Simulacion - Simulacion de ondas [[Link]](https://www.falstad.com/ripple/)
+* Video - Funcionamiento del ojo humano [[YouTube]](https://www.youtube.com/watch?v=nbwPPcwknPU)
+* Video - Funcionamiento del ojo humano [[YouTube]](https://www.youtube.com/watch?v=PmD7Tjb6yKo)
+* Video - Funcionamiento del ojo humano (problemas en la vision) [[YouTube]](https://www.youtube.com/watch?v=AsKeu4wm3XI)
+* Video - Ilusiones del color (Charla TED) [[YouTube]](https://www.youtube.com/watch?v=mf5otGNbkuc)
+* Video - Clase grabada (2021): luz, espectro, vision [[YouTube]](https://youtu.be/ffj6vZtV5gI)
+### 1.3 Adquisicion de imagenes, muestreo y cuantizacion de imagenes:
+* Colab - Sampling (muestreo espacial y de tonos de gris) [[Colab]](https://colab.research.google.com/drive/1DxVWeCfV1P-K-IyXokn49Gsv3qv-qq1C?usp=sharing)
+* Colab - Segmentacion simple a color [[Colab]](https://colab.research.google.com/drive/1aDvi_dAfqWKgXd_GnilQchBoxPXTai7O?usp=sharing)
+* PPT - Segmentacion de Frutos Rojos (idea de solucion) [[PPT]](https://github.com/domingomery/imagenes/blob/master/clases/Cap01_Introduccion/presentations/IMG01_SegmentacionFrutosRojos.pptx)
+* Colab - Trabajo en Clases E02 [[Colab]](https://colab.research.google.com/drive/10r68G48Af5ai-uU4lZ12b7Lbcfbp5MJx?usp=sharing)
+* Colab - Solucion al Trabajo en Clases E02 [[Colab]](https://colab.research.google.com/drive/11rlsVB2PNYt2ux2yilW4fyJGc_nApcDI?usp=sharing)
+* Video - Clase grabada (2021): trabajo en clases E01 [[YouTube]](https://youtu.be/7npN3o3DB30)
+### 1.4 Ejemplo:
+* Apuntes - Ejemplos en Matlab para el Toolbox Image Processing [[Apuntes]](https://github.com/domingomery/imagenes/blob/master/clases/Cap01_Introduccion/matlab/IMG01_EjemploBasicoMatlab.pdf)
+* Python - Ejemplos en Python (c) Cristian Nova S. [[Python]](https://github.com/MonkyDCristian/Prosesamiento_de_imagenes/blob/master/FPI.ipynb)
+---
