6346987e1162001e7d3c6a5b070ee97df204a5e3
Jose Ramon Iglesias Gamarra <38440709+joseramoniglesias@users.noreply.github.com>
2022-10-22 08:35:52 -0500

    Add files via upload

---
+++ clases/Cap06_Restauración_Imágenes/Matlab_Python/TRAT06_blur.m
@@ -0,0 +1,8 @@
+function G = TRAT06_blur(F,h,noise,dig)
+G = conv2(double(F),h,'valid');
+if exist('noise','var')
+   G = G+noise*randn(size(G));
+end
+if exist('dig','var')
+   G = fix(G+0.5);
+end
