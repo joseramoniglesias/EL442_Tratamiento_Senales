b4bdf3d564436577894f291215ffee35efada225
Jose Ramon Iglesias Gamarra <38440709+joseramoniglesias@users.noreply.github.com>
2024-01-15 12:02:09 -0500

    Delete clases/Cap06_Restauración_Imágenes/Matlab_Python directory

--- clases/Cap06_Restauración_Imágenes/Matlab_Python/TRAT06_blur.m
+++
@@ -1,8 +0,0 @@
-function G = TRAT06_blur(F,h,noise,dig)
-G = conv2(double(F),h,'valid');
-if exist('noise','var')
-   G = G+noise*randn(size(G));
-end
-if exist('dig','var')
-   G = fix(G+0.5);
-end
